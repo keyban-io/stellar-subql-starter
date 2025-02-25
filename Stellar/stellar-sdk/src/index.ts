703c79213d53c23e03b1fdb2043e4820b3dccaab
Maxime Vanmeerbeck <maxime.vanmeerbeck@gmail.com>
2025-02-25 11:30:42 +0100

    get account

--- Stellar/stellar-sdk/src/index.ts
+++ Stellar/stellar-sdk/src/index.ts
@@ -11,25 +11,33 @@ import {
 const rpcUrl = "http://localhost:8000/soroban/rpc";
 
 (async () => {
+  // Initialize the rpcServer
+  const RpcServer = new StellarRpc.Server(rpcUrl, { allowHttp: true });
+
+  // is RPC running fine ?
+  console.log(await RpcServer.getHealth());
+
+  // Last block (ledger)
+  const ledger = await RpcServer.getLatestLedger();
+  console.log("ledger:", ledger);
+
   // Generate a new keypair for transaction authorization.
   const keypair = Keypair.random();
   const secret = keypair.secret();
   const publicKey = keypair.publicKey();
   console.log("publicKey:", publicKey);
 
-//   await fetch(`https://friendbot-testnet.stellar.org/?addr=${publicKey}`).then(
-//     (res) => {
-//       console.log(`funded account: ${publicKey}`);
-//     },
-//   );
-
-  // Initialize the rpcServer
-  const RpcServer = new StellarRpc.Server(rpcUrl, { allowHttp: true });
-
-  console.log(await RpcServer.getHealth());
+  // Fund the account
+  await fetch(`http://localhost:8000/friendbot?addr=${publicKey}`).then(
+    (res) => {
+      console.log(`funded account: ${publicKey}`);
+    },
+  );
 
   // Load the account (getting the sequence number for the account and making an account object.)
-  //const account = await RpcServer.getAccount(publicKey);
-  const ledger = await RpcServer.getLatestLedger();
-  console.log("ledger:", ledger);
+  RpcServer.getAccount(publicKey).then((account) => {
+    console.log("sequence:", account.sequenceNumber());
+  }).catch((e) => {
+    console.log("error:", e);
+  });
 })();
