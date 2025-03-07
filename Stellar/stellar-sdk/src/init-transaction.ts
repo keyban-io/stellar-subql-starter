890bed6b6e3ee99b2e44648959f3402b7d51885f
Maxime Vanmeerbeck <maxime.vanmeerbeck@gmail.com>
2025-03-07 18:56:18 +0100

    WE DID IT \!

---
+++ Stellar/stellar-sdk/src/init-transaction.ts
@@ -0,0 +1,65 @@
+import {
+    Keypair,
+    rpc as StellarRpc,
+    Horizon,
+    scValToNative,
+    TransactionBuilder,
+    BASE_FEE,
+    Networks,
+    Operation,
+    Asset,
+    StrKey,
+  } from "@stellar/stellar-sdk";
+import { copyFileSync } from "fs";
+import { argv } from "process";
+
+const rpcUrl = "http://localhost:8000/soroban/rpc";
+
+const hexPublicKey = argv[2];
+
+(async () => {
+  // Initialize the servers
+  const RpcServer = new StellarRpc.Server(rpcUrl, { allowHttp: true });
+
+  // Remove the '0x' prefix and convert hex to buffer
+  const publicKeyBuffer = Buffer.from(hexPublicKey, 'hex');
+
+  // Convert buffer to base32-encoded string
+  const publicKeyBase32 = StrKey.encodeEd25519PublicKey(publicKeyBuffer);
+
+  // Create Keypair from public key
+  const keypair = Keypair.fromPublicKey(publicKeyBase32);
+
+  console.log("Imported Keypair Public Key:", keypair.publicKey());
+
+  // Fund the account
+  await fetch(`http://localhost:8000/friendbot?addr=${keypair.publicKey()}`).then(
+    () => {
+      console.log(`funded account: ${keypair.publicKey()}`);
+    },
+  );
+
+  // Get the balance of a public key
+  const account = await RpcServer.getAccount(keypair.publicKey());
+
+  // Create a transaction
+  const transaction = new TransactionBuilder(account, {
+    fee: BASE_FEE,
+    networkPassphrase: (await RpcServer.getNetwork()).passphrase,
+  })
+    .addOperation(Operation.payment({
+      destination: keypair.publicKey(),
+      asset: Asset.native(),
+      amount: "1",
+    }))
+    .setTimeout(86400)
+    .build();
+
+  console.log("Transaction XDR:", transaction.toXDR());
+  const transactionHash = new Uint8Array(transaction.hash().toJSON().data);
+  console.log("Transaction hash:", transactionHash);
+
+  // Convert Uint8Array to hex string
+  const transactionHashHex = Buffer.from(transactionHash).toString('hex');
+  console.log("Transaction hash (hex):", transactionHashHex);
+})();
---
+++ Stellar/stellar-sdk/src/send-transaction.ts
@@ -0,0 +1,53 @@
+import {
+    Keypair,
+    rpc as StellarRpc,
+    Horizon,
+    scValToNative,
+    TransactionBuilder,
+    BASE_FEE,
+    Networks,
+    Operation,
+    Asset,
+    StrKey,
+    Transaction,
+  } from "@stellar/stellar-sdk";
+import { argv } from "process";
+
+const rpcUrl = "http://localhost:8000/soroban/rpc";
+
+// Get the access token from environment variables
+const hexPublicKey = argv[2];
+const transactionXDR = argv[3];
+const signature = argv[4];
+
+(async () => {
+  // Initialize the servers
+  const RpcServer = new StellarRpc.Server(rpcUrl, { allowHttp: true });
+  
+  // Remove the '0x' prefix and convert hex to buffer
+  const publicKeyBuffer = Buffer.from(hexPublicKey, 'hex');
+  
+  // Convert buffer to base32-encoded string
+  const publicKeyBase32 = StrKey.encodeEd25519PublicKey(publicKeyBuffer);
+
+  // Create Keypair from public key
+  const keypair = Keypair.fromPublicKey(publicKeyBase32);
+
+  console.log("Imported Keypair Public Key:", keypair.publicKey());
+
+  // Get the balance of a public key
+  const account = await RpcServer.getAccount(keypair.publicKey());
+
+  // Convert hex signature to base64
+  const signatureBuffer = Buffer.from(signature, 'hex');
+  const signatureBase64 = signatureBuffer.toString('base64');
+
+  // Create a transaction
+  const transaction = new Transaction(transactionXDR, (await RpcServer.getNetwork()).passphrase);
+
+  transaction.addSignature(keypair.publicKey(), signatureBase64);
+
+  const response = await RpcServer.sendTransaction(transaction);
+
+  console.log(response);
+})();
